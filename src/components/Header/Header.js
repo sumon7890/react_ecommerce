0e442b59f55313fadba00d41f2a8cd844d9f54bc
sumon7890 <sumon.ct.2019@gmail.com>
2021-06-11 23:48:29 +0600

    Create Sample React Project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Hedar.css';
+
+const Header = () => {
+    return (
+        <div className="header" >
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
